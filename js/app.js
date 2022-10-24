70848bf3cc553d69723f02f608db7700fc654248
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-24 13:59:10 +0600

    added color

--- js/app.js
+++ js/app.js
@@ -7,18 +7,17 @@ const loadCatagories = () => {
 }
 const displayCatagories = (catagories) => {
     // console.log(catagories);
-    
+
     const caragorySection = document.getElementById('catagory_section');
 
     catagories.forEach(catagori => {
         const caragoryDiv = document.createElement('div');
         caragoryDiv.innerHTML = `
         <div>
-            <button onclick="loadNewsDetails('${catagori.category_id}')" id="" class=" border border-0 p-2 rounded text-secondary">${catagori.category_name}</button>
-
+            <button onclick="loadNewsDetails('${catagori.category_id}')" style="background-color:#EEEFFF;color: #5D5FEF;" class=" border border-0  rounded ">${catagori.category_name}</button>
         </div>
     `;
-    caragorySection.appendChild(caragoryDiv)
+        caragorySection.appendChild(caragoryDiv)
     });
 
 
@@ -39,21 +38,21 @@ const displayNewsDetails = (newsDetails) => {
         console.log(newsDetail);
         const newsDetailDiv = document.createElement('div');
         newsDetailDiv.innerHTML = `
-        <div class="d-flex mt-5"> 
-        <div>
+        <div class="d-flex mt-4 bg-white p-4 rounded"> 
+        <div class="">
             <img class="img-fluid rounded" src="${newsDetail.thumbnail_url}" alt="thamnnail img">
         </div>
         <div class="ms-3">
-            <h4>${newsDetail.title}</h4>
-            <p class="text-secondary">${newsDetail.details.slice(0,250)}...</p>
-            <div class="d-flex justify-content-between align-items-center">
+            <h4 class="container">${newsDetail.title}</h4>
+            <p class="text-secondary container">${newsDetail.details.slice(0, 550)}...</p>
+            <div class="container d-flex justify-content-between align-items-center">
                 <div class="d-flex">
                     <div class="p-1">
-                        <img style="width: 40px" class="img-fluid rounded-circle" src="${newsDetail.author.img}" alt="writer img">
+                        <img style="width:40px" class=" img-fluid rounded-circle " src="${newsDetail.author.img}" alt="writer img">
                     </div>
                     <div class="ms-1">
                         <h6>${newsDetail.author.name}</h6>
-                        <p class="text-secondary">${newsDetail.author.published_date.slice(0,11)}</p>
+                        <p class="text-secondary">${newsDetail.author.published_date.slice(0, 11)}</p>
                     </div>
                 </div>
                 <h6>Total Views: ${newsDetail.total_view ? newsDetail.total_view : 'no View'}</h6>
