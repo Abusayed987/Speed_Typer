abd80268bb470233be7fcb0980193d38f55c8517
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-24 11:33:14 +0600

    loaded news catagories

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('connected');
+// caragories section 
+const loadCatagories = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCatagories(data.data.news_category));
+}
+const displayCatagories = (catagories) => {
+        console.log(catagories);
+    
+        const caragorySection = document.getElementById('catagory_section');
+
+    catagories.forEach(catagori => {
+        const caragoryDiv = document.createElement('div');
+        caragoryDiv.innerHTML = `
+        <div>
+            <button onclick="" class=" border border-0 p-2 rounded text-secondary">${catagori.category_name}</button>
+
+        </div>
+    `;
+    caragorySection.appendChild(caragoryDiv)
+    });
+
+
+}
+
+loadCatagories();
